09257f4c2d0f2a48403197cbbfda92c456d2a485
tevtonez <tevtonez@gmail.com>
2017-02-09 10:22:55 +0100

    add task 14 solution

---
+++ 14_List_Remove_Duplicates.py
@@ -0,0 +1,38 @@
+'''
+----------------------------------------
+TASK 14 from http://www.practicepython.org/
+----------------------------------------
+Write a program (function!) that takes a list and returns a new list that contains all
+the elements of the first list minus all the duplicates.
+
+Extras:
+1. Write two different functions to do this - one using a loop and constructing a list,
+and another using sets.
+2. Go back and do Exercise 5 using sets, and write the solution for that in a different
+function.
+
+'''
+
+# SOLUTION
+# created by Kostya
+# on 9 Feb 2017
+# Time spent: 20 min
+
+
+from random import randrange
+
+if __name__ == '__main__':
+
+    def list_to_set( _list ):
+        """takes a list and makes set"""
+        return set( _list )
+
+    def generate_list( n ):
+        """generates list with repeatalbe random numbers"""
+        l1 = []
+        for _ in range( n ):
+            num = randrange( n )
+            l1.append( num )
+        return l1
+
+    print ( list_to_set( generate_list( 20 ) ) )
