49f6effffdc2f64a6aacbdc141eac619d0dad222
tevtonez <tevtonez@gmail.com>
2017-02-11 11:07:27 +0100

    refactor task 16

--- 16_password_generator.py
+++ 16_password_generator.py
@@ -18,62 +18,24 @@ Extra:
 # on 10 Feb 2017
 # Time spent: 44 min
 
-from random import *
+import random
 import string
 
 if __name__ == '__main__':
 
 
     def gen_pass( strength ):
-        numb_list = range( 10 )
-        chars_list = string.ascii_lowercase
-        password = []
+        '''Takes number as an argument and generates password with desired length'''
+        chars_list = string.ascii_letters + string.digits + string.punctuation
+        print ( "".join( random.choice( chars_list ) for i in range( int( strength ) ) ) )
 
 
-        if strength == "w":
-            # generating weak numeric password
-
-            for i in range( 6 ):
-                password.append( str( choice( numb_list ) ) )
-
-            password_res = "".join( password )
-            print ( password_res )
-
-
-        elif strength == "m":
-            # generating medium alpha-numeric password
-            for i in range( 5 ):
-                password.append( str( choice( numb_list ) ) )
-                password.append( choice( chars_list ) )
-
-            shuffle( password )
-            password_res = "".join( password )
-            print ( password_res )
-
-
-        else:
-            # generating strong alpha-numeric case-sensitive password
-            for i in range( 7 ):
-                password.append( str( choice( numb_list ) ) )
-                upper_case = choice( [0, 1] )
-                if upper_case:
-                    password.append( choice( chars_list.upper() ) )
-                else:
-                    password.append( choice( chars_list ) )
-                password.append( choice( chars_list ) )
-
-            shuffle( password )
-            password_res = "".join( password )
-            print ( password_res )
-
-
-
-
-    answers = ["s", "w", "m", ""]
-    pass_strength = input( "\nHow strong password should be? (Strong [S], Medium [M], Weak [W])\nDefault [S]trong: " )
-
-    while pass_strength.lower() not in answers:
-        pass_strength = input( "\nHow strong password should be? (Strong [S], Medium [M], Weak [W])\nDefault [S]trong: " )
-
+    #===========================================================================
+    # EXECUTION
+    #===========================================================================
+    pass_strength = input( "\nEnter desired length of the password: " )
+    while True:
+        if pass_strength:
+            break
 
     gen_pass( pass_strength )
