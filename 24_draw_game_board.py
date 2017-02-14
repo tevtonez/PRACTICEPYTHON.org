e62e044263a18166f484590ee097fc8e0ae6b1a7
tevtonez <tevtonez@gmail.com>
2017-02-14 17:42:26 +0100

    fix task24

---
+++ 24_draw_game_board.py
@@ -0,0 +1,25 @@
+'''
+----------------------------------------
+TASK 21 from http://www.practicepython.org/
+----------------------------------------
+Take the code from the How To Decode A Website exercise (if you didn’t do it or just want to
+play with some different code, use the code from the solution), and instead of printing the
+results to a screen, write the results to a txt file. In your code, just make up a name for
+the file you are saving to.
+
+Extras:
+
+Ask the user to specify the name of the output file that will be saved.
+
+'''
+
+#
+# SOLUTION
+# created by Kostya
+# on XX Xxx 2017
+# Time spent: XX min
+#
+
+
+if __name__ == '__main__':
+
