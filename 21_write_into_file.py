61fe6dcc87536ce1279efc52aab71c1f44e08f8e
tevtonez <tevtonez@gmail.com>
2017-02-20 11:13:47 +0100

    add solution to task 21

--- 21_write_into_file.py
+++ 21_write_into_file.py
@@ -2,53 +2,45 @@
 ----------------------------------------
 TASK 21 from http://www.practicepython.org/
 ----------------------------------------
-This exercise is Part 1 of 4 of the Tic Tac Toe exercise series.
-The other exercises are: Part 2, Part 3, and Part 4.
-
-Time for some fake graphics! Let’s say we want to draw game boards that look like this:
-
- --- --- ---
-|   |   |   |
- --- --- ---
-|   |   |   |
- --- --- ---
-|   |   |   |
- --- --- ---
-This one is 3x3 (like in tic tac toe). Obviously, they come in many other sizes
-(8x8 for chess, 19x19 for Go, and many more).
-
-Ask the user what size game board they want to draw, and draw it for them to the
-screen using Python’s print statement.
+Take the code from the How To Decode A Website #17 exercise and instead of printing the results to a screen,
+write the results to a txt file. In your code, just make up a name for the file you are saving to.
 
 Extras:
 
-Ask the user to specify the name of the output file that will be saved.
+1. Ask the user to specify the name of the output file that will be saved.
 
 '''
 
 #
 # SOLUTION
 # created by Kostya
-# on 14 Feb 2017
-# Time spent: 20 min
+# on 20 Feb 2017
+# Time spent: 15 min
 #
 
-def draw_line( cols, rows ):
 
-    a = " ---"
-    b = "|   "
 
-    for row in range( int( rows ) ):
-        print( a * int( cols ) )
-        print( b * int( cols ) + "|" )
-        print( a * int( cols ) )
-
+import urllib, html.parser
+from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
 
-    cols = input( "Enter number of columns: " )
-    rows = input( "Enter number of rows: " )
+    file_name = input( "Enter desired file name: " )
+    file_name += ".txt"
 
-    draw_line( cols, rows )
 
+    url = "http://www.nytimes.com/"
+
+    with urllib.request.urlopen( url ) as response:
+        html_doc = response.read()
+
+    soup = BeautifulSoup( html_doc, 'html.parser' )
+
+    with open( file_name, 'w', encoding = 'utf-8', newline = "\n" ) as file:
+        try:
+            for title in soup.select( 'h2.story-heading' ):
+                file.write( title.get_text().strip() + "\n" )
+            print( "File '{}' written successfully!".format( file_name ) )
+        except:
+            print( "Failed writing into a file '{}'.".format( file_name ) )
