47f968c9b9b9d43d39d54503f23bb35c1615c1e2
tevtonez <tevtonez@gmail.com>
2017-02-21 17:26:28 +0100

    add extra solution to task 22

--- 22_read_from_file.py
+++ 22_read_from_file.py
@@ -1,6 +1,6 @@
 '''
 ----------------------------------------
-TASK 21 from http://www.practicepython.org/
+TASK 22 from http://www.practicepython.org/
 ----------------------------------------
 Given a task22-1.txt file that has a list of a bunch of names, count how many of each name there are in the file,
 and print out the results to the screen.
@@ -20,37 +20,65 @@ I talked a little bit about it in this post (http://www.practicepython.org/exerc
 # created by Kostya
 # on 21 Feb 2017
 # Time spent: 14 min without extra
-# additional xx min for extra task
+# additional 16 min for extra task
 #
 
 
-
-def read_file_content( file_name ):
-
-    # read the file into content var
-    with open( file_name, 'r', encoding = 'utf-8' ) as file:
-        content = file.readlines()
-
-    # create dictionary
-    names = {}
-
-    # read from content and fill in the dictionary, counting names
-    for line in content:
-
-        # strip new lines chars
-        line = line.strip()
-
-        # fill in the dictionary with names and numbers
-        if line in names.keys():
-            names[line] += 1
-        else:
-            names[line] = 1
-
-    return names
-
-
-
 if __name__ == '__main__':
+
+    import re
+
+    def read_file_count_names( file_name ):
+
+        # read the file into content var
+        with open( file_name, 'r', encoding = 'utf-8' ) as file:
+            content = file.readlines()
+
+        # create dictionary
+        names = {}
+
+        # read from content and fill in the dictionary, counting names
+        for line in content:
+
+            # strip new lines chars
+            line = line.strip()
+
+            # fill in the dictionary with names and numbers
+            if line in names.keys():
+                names[line] += 1
+            else:
+                names[line] = 1
+
+        return names
+
+
+    def read_file_count_cats( file_name ):
+
+        # read the file into content var
+        with open( file_name, 'r', encoding = 'utf-8' ) as file:
+            content = file.readlines()
+
+        # create dictionary
+        categories = {}
+
+        # read from content and fill in the dictionary, counting names
+        for line in content:
+
+            line = line.strip()
+
+            # get the part of line we need
+            patt = re.compile( r'\/([A-z]{2,})\/' )
+            category = re.search( patt, line )
+
+            # fill in the dictionary with names and numbers
+            if category.groups()[0] in categories.keys():
+                categories[category.groups()[0]] += 1
+            else:
+                categories[category.groups()[0]] = 1
+
+        return categories
+
+
 
 #===============================================================================
 # task22-1.txt file
@@ -58,8 +86,9 @@ if __name__ == '__main__':
 
 
     file_name = "task22-1.txt"
-
-    for name, quantity in read_file_content( file_name ).items():
+    for name, quantity in read_file_count_names( file_name ).items():
         print ( "{} - {}".format( name, quantity ) )
 
-
+    file2 = "task22-2.txt"
+    for category, quantity in read_file_count_cats( file2 ).items():
+        print ( "{} - {}".format( category, quantity ) )
